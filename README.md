e809d56808dd915fdefa6d48768507282aa95ffb
Makubob <makupi@icloud.com>
2018-11-16 14:10:56 +0100

    Adds config.json for costum configurations

--- README.md
+++ README.md
@@ -2,16 +2,28 @@
 
 A small tool to potentially help emote artists resize their images faster/easier.
 
-## Sizes
+## Config
 
-Currently the following twitch emote sizes are used, will be configerable in the future.
-All sizes in pixels.
-
-    112x112  
-    56x56
-    28x28
+The folders and sizes can be configured in the `config.json` file.   
+Structure of the file:
+```json
+{
+    "twitchEmotes": {
+        "sizes": [112, 56, 28],
+        "inputFolder": "in_emotes", 
+        "outputFolder": "out_emotes"
+    },
+    "subBadges": {
+        "sizes": [72, 36, 18],
+        "inputFolder": "in_badges",
+        "outputFolder": "out_badges"
+    }    
+}
+```
+`twitchEmotes` and `subBadges` are the default configurations.    
+Each key has an object with the `sizes` as an array of intergers and two strings with `inputFolder` and `outputFolder`.
 
 ## Usage
 
-- Put all your .png files that you want to be resized in the `input` folder and run the `resize.exe`.     
-- It will automatically resize them to the sizes mentioned above and put them into the `output` folder named as `file_size.png` (e.g. Kappa_112.png).
+- Put all your .png files that you want to be resized in the `in_emotes` folder and run the `resize.exe`.     
+- It will automatically resize them to the sizes mentioned above and put them into the `out_emotes` folder named as `file_size.png` (e.g. Kappa_112.png).
--- resize.py
+++ resize.py
@@ -1,30 +1,55 @@
 from PIL import Image
-import glob, os
+import glob, os, json
+
+
+default_config = {
+        "twitchEmotes": {
+            "sizes": [112, 56, 28],
+            "inputFolder": "in_emotes", 
+            "outputFolder": "out_emotes"
+        },
+        "subBadges": {
+            "sizes": [72, 36, 18],
+            "inputFolder": "in_badges",
+            "outputFolder": "out_badges"
+            }    
+        }
+try:
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+except:  # fallback default config
+    config = default_config
+
 linebreaker = "-----------------------------------------------"
 print(linebreaker)
 print("## Running resize tool by maku :3")
 print("## For issues/feedback contact me via discord maku#0001")
 print(linebreaker)
 
-sizes = [112, 56, 28]
-files = glob.glob("input/*.png")
+for key, value in config.items():
+    sizes = value['sizes']
+    in_folder = value.get('inputFolder', 'input')
+    out_folder = value.get('outputFolder', 'output')
+    files = glob.glob(in_folder + "/*.png")
 
-print("Output sizes: " + str(sizes))
-print("Number of input files found: " + str(len(files)))
-print("Starting resize..")
-print(linebreaker)
+    print("Output sizes: " + str(sizes))
+    print("Number of input files found: " + str(len(files)))
+    print("Starting resize " + key + "..")
+    print(linebreaker)
 
-for infile in files:
-    file, ext = os.path.splitext(os.path.basename(infile))
-    print("Resizing " + file + ".. ", end='')
-    im = Image.open(infile)
-    for size in sizes:
-        res = im.resize((size, size), Image.ANTIALIAS)
-        filename = "output/" + file + "_" + str(size) + ".png"
-        res.save(filename, "PNG", quality=95)
-    print("done.")
+    for infile in files:
+        file, ext = os.path.splitext(os.path.basename(infile))
+        print("Resizing " + file + ".. ", end='')
+        im = Image.open(infile)
+        for size in sizes:
+            res = im.resize((size, size), Image.ANTIALIAS)
+            filename = out_folder + "/" + file + "_" + str(size) + ".png"
+            res.save(filename, "PNG", quality=95)
+        print("done.")
+    print(key + " done.")
+    print(linebreaker)
 
-print(linebreaker)
+
 print("All files processed and saved to output folder.")
 
 input("Press ENTER to exit..")
